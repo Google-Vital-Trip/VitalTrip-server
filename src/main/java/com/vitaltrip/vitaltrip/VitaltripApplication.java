7de8af64549404c93cddb277e213c94569debf3b
dkswoals <a4445656@gmail.com>
2025-07-22 18:52:29 +0900

    init project

---
+++ src/main/java/com/vitaltrip/vitaltrip/VitaltripApplication.java
@@ -0,0 +1,13 @@
+package com.vitaltrip.vitaltrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaltripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaltripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitaltrip/vitaltrip/VitaltripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitaltrip.vitaltrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaltripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
