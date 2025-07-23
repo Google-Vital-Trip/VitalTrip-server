12c54998794f3b20e6c61b945deff5aaa98d3a03
dkswoals <a4445656@gmail.com>
2025-07-24 04:05:00 +0900

    feat: BaseEntity Id 컬럼 추가

--- src/main/java/com/vitaltrip/vitaltrip/domain/BaseEntity.java
+++ src/main/java/com/vitaltrip/vitaltrip/domain/BaseEntity.java
@@ -2,6 +2,9 @@ package com.vitaltrip.vitaltrip.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -18,6 +21,10 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @CreatedDate
     @Column(updatable = false, nullable = false)
