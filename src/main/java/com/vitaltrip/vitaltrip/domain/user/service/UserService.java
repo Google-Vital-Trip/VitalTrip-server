b77e7cb1364fc9dba9a3a9073b36ff0870b707bf
dkswoals <a4445656@gmail.com>
2025-08-22 11:09:38 +0900

    feat: UserController 구현 및 서비스 로직 작성

---
+++ src/main/java/com/vitaltrip/vitaltrip/domain/user/service/UserService.java
@@ -0,0 +1,44 @@
+package com.vitaltrip.vitaltrip.domain.user.service;
+
+import com.vitaltrip.vitaltrip.domain.user.User;
+import com.vitaltrip.vitaltrip.domain.user.dto.ProfileUpdateRequest;
+import com.vitaltrip.vitaltrip.domain.user.dto.UserInfoResponse;
+import com.vitaltrip.vitaltrip.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserInfoResponse getUserInfo(User user) {
+        return new UserInfoResponse(
+                user.getId(),
+                user.getEmail(),
+                user.getName(),
+                user.getBirthDate(),
+                user.getCountryCode(),
+                user.getPhoneNumber(),
+                user.getProfileImageUrl()
+        );
+    }
+
+    @Transactional
+    public void updateProfile(User user, ProfileUpdateRequest request) {
+        user.updateProfile(
+                request.name(),
+                request.birthDate(),
+                request.countryCode(),
+                request.phoneNumber()
+        );
+
+        userRepository.save(user);
+    }
+
+}
