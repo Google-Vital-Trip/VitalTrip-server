de41be7969873d4118c7f99e6429812a58b0e920
dkswoals <a4445656@gmail.com>
2025-12-23 21:04:49 +0900

    Refactor: swagger 설정 수정

--- src/main/java/com/vitaltrip/vitaltrip/config/SwaggerConfig.java
+++ src/main/java/com/vitaltrip/vitaltrip/config/SwaggerConfig.java
@@ -65,10 +65,7 @@ public class SwaggerConfig {
                     .url("http://localhost:8080")
                     .description("로컬 개발 서버"),
                 new Server()
-                    .url("http://dkswoalstest.duckdns.org")
-                    .description("개발 환경 서버"),
-                new Server()
-                    .url("https://dkswoalstest.duckdns.org")
+                    .url("https://api.aivitaltrip.com")
                     .description("개발 환경 서버(https)")
             ))
             .components(new Components()
