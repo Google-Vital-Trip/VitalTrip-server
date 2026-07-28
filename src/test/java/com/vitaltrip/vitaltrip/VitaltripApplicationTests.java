8d71cd60b70a1b605a9e6cfc27f2b554d9a1b9de
dkswoals <a4445656@gmail.com>
2026-07-28 13:57:17 +0900

    refactor: test mysql 컨테이너 추가 및 적용

--- src/test/java/com/vitaltrip/vitaltrip/VitaltripApplicationTests.java
+++ src/test/java/com/vitaltrip/vitaltrip/VitaltripApplicationTests.java
@@ -2,10 +2,13 @@ package com.vitaltrip.vitaltrip;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import support.TestcontainersConfig;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestcontainersConfig.class)
 class VitaltripApplicationTests {
 
 	@Test
